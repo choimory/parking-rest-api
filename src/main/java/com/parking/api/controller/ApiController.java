aedc605fd50a10b6fbf145bdf2b929dade17ec6c
choimory <43328589+choimory@users.noreply.github.com>
2021-04-14 22:20:29 +0900

    1

--- src/main/java/com/parking/api/controller/ApiController.java
+++ src/main/java/com/parking/api/controller/ApiController.java
@@ -4,8 +4,12 @@ import com.parking.api.service.ApiService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
@@ -14,7 +18,15 @@ public class ApiController {
     private final ApiService apiService;
 
     @GetMapping("/api")
-    public String api(){
+    public String api(Model model, String url){
+        LOGGER.info("ApiController.api() called -> {}", url);
+
+        try{
+            String xml = apiService.getStringXmlFromApi(5000, url, HttpMethod.GET);
+            Map<String, Object> json = apiService.parseStringXmlToJson(xml);
+        }catch (Exception e){
+
+        }
 
         return "api/list";
     }
--- src/main/java/com/parking/api/service/ApiServiceImpl.java
+++ src/main/java/com/parking/api/service/ApiServiceImpl.java
@@ -1,9 +1,11 @@
 package com.parking.api.service;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public class ApiServiceImpl implements ApiService{
     @Override
     public String getStringXmlFromApi(int timeout, String url, HttpMethod httpMethod) {
